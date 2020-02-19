d187ccd0fdbe4923b540004fdc0c28de753fb02e
Mortyshka <49131444+Mortyshka@users.noreply.github.com>
2020-02-19 20:59:55 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -260,7 +260,7 @@ async def play(ctx):
         await Bot.change_presence(activity=None)
 
 
-#token=os.environ.get('BOT_TOKEN')   # для сервера, чтобы никто не видел токен
-#Bot.run(str(token))
+token=os.environ.get('BOT_TOKEN')   # для сервера, чтобы никто не видел токен
+Bot.run(str(token))
 
-Bot.run(open('token.txt','r').readline())  # чтение токена из файла
+#Bot.run(open('token.txt','r').readline())  # чтение токена из файла
